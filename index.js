43b002bf60feff8902a724fe5063b867055b2a84
Kunjesh <kunjesh7383@gmai.com>
2023-06-28 11:44:30 +0530

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 3200
+
+app.use(cors())
+const port = process.env.PORT || 5000
 
 const apiData = require('./data.json')
-
-// app.use(cors)
 
 app.get("/", (req,res)=>{
     res.send("hello I am live")
 })
 
-app.get("/data", (req,res)=>{
+app.get("/data" , (req,res)=>{
+    console.log("API Hosted")
     res.send(apiData)
 })
 
-
 app.listen(port, ()=>{
-    console.log("server running ");
+    console.log(`server running on PORT : ${port}` );
 })
